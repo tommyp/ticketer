c84ec7381ad09bde53e68b2d1af748c31cbc855b
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-02-16 16:28:57 +0000

    update landing spec to match change of routes

--- spec/requests/landing_request_spec.rb
+++ spec/requests/landing_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Landings", type: :request do
 
-  describe "GET /index" do
+  describe "GET /" do
     it "returns http success" do
-      get "/landing/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
