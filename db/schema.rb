90be75ac5b88c9435c9f470a4e75e0023096041a
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-02-24 23:53:11 +0000

    add stripe integration
    This commit adds stripe integration and /guests endpoint in order to
    reset the application.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_124205) do
+ActiveRecord::Schema.define(version: 2020_02_23_162212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
