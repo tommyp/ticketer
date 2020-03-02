cea9e92c27e943ac918c9a9af8f20fb2cdfae959
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-03-02 20:02:53 +0000

    add sidekiq to clean up claimed but not sold tickets
    As tickets get claimed but not sold, we need to make them available
    again. This PR adds a TicketCleanupWorker that frees up tickets that
    have been claimed but not sold, 5 minutes after they are claimed.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -15,10 +15,17 @@ class Ticket < ApplicationRecord
     self.name = customer_name
     self.status = :claimed
     save!
+    TicketCleanupWorker.perform_in(5.minutes, self.id)
   end
 
   def sell!
     self.status = "sold"
+    save!
+  end
+
+  def cleanup!
+    self.status = "available"
+    self.name = nil
     save!
   end
 
